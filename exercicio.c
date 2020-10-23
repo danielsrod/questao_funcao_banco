7a2a230ae38bacb3e8ccc47e9d8a9c055e9938e1
Daniel Rodrigues <61955775+dxnielsr@users.noreply.github.com>
2020-10-23 02:31:36 -0400

    exercício finalizado c:

--- exercicio.c
+++ exercicio.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
 
-void verSaldo(float saldo) {
-    return printf("Seu saldo é: R$ %.2f", saldo);
+float verSaldo(float saldo) {
+    return printf("Seu saldo eh: R$ %.2f", saldo);
 }
 
-void fazerDeposito(float saldo, float dep) {
-    printf("Quanto você quer depositar?");
+float fazerDeposito(float saldo, float dep) {
+    printf("Quanto voce quer depositar?");
     printf("\nR$ ");
     scanf("%f", &dep);
-    saldo = saldo + dep;
+    printf("Saldo de R$%.2f depositado com sucesso.", dep);
 
-    return printf("Saldo de R$%.2f depositado com sucesso.", dep);   
+    return saldo + dep;
 }
 
-void fazerSaque(float saldo, float saque) {
-    printf("Quando você quer sacar?");
+float fazerSaque(float saldo, float saque) {
+    printf("Quando voce quer sacar?");
     printf("\nR$ ");
     scanf("%f", &saque);
-    saldo = saldo - saque;
+    printf("Valor de R$ %.2f sacado com sucesso.", saque);
 
-    return printf("Valor de R$ %.2f sacado com sucesso.", saque);
+    return saldo - saque; 
 }
 
 
 int main () {
     float saldo = 1000, dep = 0, saque = 0;
-    int opc = 0;
+    int opc = 0, cont = 1;
+
+
+    do {
 
     printf("\n[1] - VER SALDO\n[2] - FAZER DEPOSITO\n[3] - FAZER SAQUE\n\n");
     printf("Escolha: ");
@@ -35,14 +38,23 @@ int main () {
         // VER SALDO
         verSaldo(saldo);
     } else if (opc == 2) {
-        fazerDeposito(saldo, dep);
+        saldo = fazerDeposito(saldo, dep);
         // FAZER DEPOSITO
     } else if (opc == 3) {
         // FAZER SAQUE
-        fazerSaque(saldo, saque);
+        saldo = fazerSaque(saldo, saque);
     } else {
-        printf("Comando inválido.");
+        printf("Comando invalido.");
     }
+
+    printf("\n\nDeseja fazer mais alguma operacao?");
+    printf("\n[1] - SIM\n[2] - NAO");
+    printf("\nEscolha: ");
+    scanf("%d", &cont);
+
+    } while (cont == 1);
+
+    
     
 
     return 0;
