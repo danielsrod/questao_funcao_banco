a45a208a3010f7508a5efb22824362fda4ae8dbc
Daniel Rodrigues <61955775+dxnielsr@users.noreply.github.com>
2020-10-23 01:58:05 -0400

    estrutura inicial montada

---
+++ exercicio.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <locale.h>
+
+void verSaldo() {
+
+}
+
+void fazerDeposito() {
+
+}
+
+void fazerSaque() {
+
+}
+
+
+int main () {
+    //printf("\nSALVE MUNDO!\n\n");
+}
